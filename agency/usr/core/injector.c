61761fa81f995be87ba0465191d7ffb1ca90c551
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-05-22 10:37:43 +0200

    Fixed issues related to usage of /dev/soo between migration and upgrader

--- agency/usr/core/injector.c
+++ agency/usr/core/injector.c
@@ -30,21 +30,20 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <dirent.h>
+
 #include <sys/ioctl.h>
 #include <sys/mman.h>
+#include <sys/stat.h>
 
 #include <core/core.h>
 #include <core/inject.h>
 #include <core/debug.h>
 #include <core/types.h>
+#include <core/device_access.h>
 
 #include <uapi/dcm.h>
 
 #include <injector/core.h>
-
-
-static int fd_core;
-
 
 /**
  * Inject a ME.
@@ -56,7 +55,7 @@ int inject_ME(void *ME_buffer) {
 
 	args.buffer = ME_buffer;
 
-	if ((rc = ioctl(fd_migration, AGENCY_IOCTL_INJECT_ME, &args)) < 0) {
+	if ((rc = ioctl(fd_core, AGENCY_IOCTL_INJECT_ME, &args)) < 0) {
 		printf("Failed to inject ME (%d)\n", rc);
 		BUG();
 	}
@@ -89,6 +88,90 @@ void ME_inject(unsigned char *ME_buffer) {
 
 	/* Be ready for future migration */
 	set_personality_initiator();
+}
+
+
+void agency_inject_ME_from_memory(void) {
+}
+
+void sig_inject_ME_from_memory(int sig) {
+	agency_inject_ME_from_memory();
+}
+
+/**
+ * Look for MEs in the SOO_ME_DIRECTORY directory and inject the MEs one by one.
+ * The SOO_ME_DIRECTORY can be a mount point (mounted on a dedicated storage partition,
+ * this is the default method) or a directory integrated into the agency's rootfs.
+ */
+void inject_MEs_from_filesystem(void) {
+	DIR *directory;
+	int fd;
+	struct dirent *ent;
+	char filename[256];
+	int nread, ME_size;
+	unsigned char *ME_buffer = NULL;
+	struct stat filestat;
+
+	if (opt_noinject) {
+		DBG0("-noinject enabled: Skipping auto injection\n");
+		return;
+	}
+
+	directory = opendir(SOO_ME_DIRECTORY);
+	if (!directory) {
+		DBG0("The " SOO_ME_DIRECTORY "does not exist\n");
+
+		return;
+	}
+
+	while ((ent = readdir(directory)) != NULL) {
+
+		/* Ignore . and .. */
+		if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..") || !strcmp(ent->d_name, "lost+found"))
+			continue;
+
+		printf("Found new ME to inject : %s\n", ent->d_name);
+
+		sprintf(filename, SOO_ME_DIRECTORY "/%s", ent->d_name);
+
+		stat(filename, &filestat);
+
+		fd = open(filename, O_RDONLY);
+
+		if (fd < 0) {
+			perror(filename);
+			printf("%s not found.\n", filename);
+			exit(-1);
+		}
+
+		ME_size = filestat.st_size;
+
+		/* Allocate the ME buffer */
+		ME_buffer = malloc(ME_size);
+		if (!ME_buffer) {
+			printf("%s: failure during ME malloc...\n", __func__);
+			BUG();
+		}
+
+		DBG("agency_core: size to read from sd : %d, buffer address : 0x%08x\n", ME_size, (unsigned int) ME_buffer);
+
+		/* Read the ME content  */
+		nread = read(fd, ME_buffer, ME_size);
+		if (nread != ME_size) {
+			printf("Error when reading the ME\n");
+			BUG();
+		}
+
+		/* Inject the ME */
+		ME_inject(ME_buffer);
+
+		close(fd);
+
+		free(ME_buffer);
+
+	}
+
+	closedir(directory);
 }
 
 
@@ -132,7 +215,10 @@ void *ME_retrieve_fn(void *dummy) {
 
 			printf("Injector: An ME is ready to be retrieved (%d B)\n", args.size);
 			ME = malloc(args.size);
-
+			if (!ME) {
+				printf("%s: failure during malloc...\n", __func__);
+				BUG();
+			}
 
 			while (current_size != args.size) {
 				br = read(fd_core, ME+current_size, chunk);
