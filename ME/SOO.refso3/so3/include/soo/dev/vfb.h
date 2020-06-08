22284b1e4f3aa607a25bdf16799888f0ad9153e4
Nikolaos Garanis <945263-nyg@users.noreply.gitlab.com>
2020-06-08 21:56:45 +0200

    refso3: base vfbfront on the new vdummy (see #5).

--- ME/SOO.refso3/so3/include/soo/dev/vfb.h
+++ ME/SOO.refso3/so3/include/soo/dev/vfb.h
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2020 Nikolaos Garanis <nikolaos.garanis@heig-vd.ch>
  * Copyright (C) 2014-2019 Daniel Rossier <daniel.rossier@heig-vd.ch>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -20,10 +19,9 @@
 #ifndef VFB_H
 #define VFB_H
 
-#include <device/irq.h>
-
 #include <soo/ring.h>
 #include <soo/grant_table.h>
+#include <soo/vdevfront.h>
 
 #define VFB_PACKET_SIZE	32
 
@@ -39,44 +37,29 @@ typedef struct  {
 } vfb_response_t;
 
 /*
- * Generate blkif ring structures and types.
+ * Generate ring structures and types.
  */
 DEFINE_RING_TYPES(vfb, vfb_request_t, vfb_response_t);
 
-typedef struct {
+/*
+ * General structure for this virtual device (backend side)
+ */
 
-	struct vbus_device  *dev;
+typedef struct {
+	vdevfront_t vdevfront;
 
 	vfb_front_ring_t ring;
+	unsigned int irq;
+
 	grant_ref_t ring_ref;
 	grant_handle_t handle;
 	uint32_t evtchn;
-	uint32_t irq;
 
 } vfb_t;
 
-extern vfb_t vfb;
-
-/* ISR associated to the ring */
-irq_return_t vfb_interrupt(int irq, void *data);
-
-/* State management */
-void vfb_probe(void);
-void vfb_closed(void);
-void vfb_suspend(void);
-void vfb_resume(void);
-void vfb_connected(void);
-void vfb_reconfiguring(void);
-void vfb_shutdown(void);
-
-void vfb_vbus_init(void);
-
-/* Processing and connected state management */
-void vfb_start(void);
-void vfb_end(void);
-bool vfb_is_connected(void);
-
-/**/
-void write_vbstore(void);
+static inline vfb_t *to_vfb(struct vbus_device *vdev) {
+	vdevfront_t *vdevback = dev_get_drvdata(vdev->dev);
+	return container_of(vdevback, vfb_t, vdevfront);
+}
 
 #endif /* VFB_H */
--- ME/SOO.refso3/so3/soo/drivers/vfbfront/vfb.c
+++ ME/SOO.refso3/so3/soo/drivers/vfbfront/vfb.c
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2020 Nikolaos Garanis <nikolaos.garanis@heig-vd.ch>
+ * Copyright (C) 2018-2019 Daniel Rossier <daniel.rossier@soo.tech>
+ * Copyright (C) 2018-2019 Baptiste Delporte <bonel@bonel.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -23,10 +25,14 @@
 #include <heap.h>
 #include <mutex.h>
 #include <delay.h>
+#include <memory.h>
+#include <asm/mmu.h>
 
 #include <device/driver.h>
+#include <device/fb/so3virt_fb.h>
 
 #include <soo/evtchn.h>
+#include <soo/gnttab.h>
 #include <soo/hypervisor.h>
 #include <soo/vbus.h>
 #include <soo/console.h>
@@ -34,22 +40,22 @@
 
 #include <soo/dev/vfb.h>
 
-vfb_t vfb;
 
 static bool thread_created = false;
 
-irq_return_t vfb_interrupt(int irq, void *dev_id)
-{
-	vfb_response_t *vfb_response;
-
-	if (!vfb_is_connected())
-		return IRQ_COMPLETED;
+irq_return_t vfb_interrupt(int irq, void *dev_id) {
+	struct vbus_device *vdev = (struct vbus_device *) dev_id;
+	vfb_t *vfb = to_vfb(vdev);
+	vfb_response_t *ring_rsp;
 
 	DBG("%s, %d\n", __func__, ME_domID());
 
-	vfb_response = vfb_ring_response(&vfb.ring);
+	while ((ring_rsp = vfb_ring_response(&vfb->ring)) != NULL) {
 
-	/* Do something... */
+		DBG("%s, cons=?\n", __func__);
+
+		/* Do something with the response */
+	}
 
 	return IRQ_COMPLETED;
 }
@@ -58,64 +64,186 @@ irq_return_t vfb_interrupt(int irq, void *dev_id)
  * The following function is given as an example.
  *
  */
-void vfb_generate_request(char *buffer)
-{
+#if 0
+void vfb_generate_request(char *buffer) {
 	vfb_request_t *ring_req;
 
-	vfb_start();
+	vdevfront_processing_start();
 
 	/*
 	 * Try to generate a new request to the backend
 	 */
+	if (!RING_FULL(&vfb.ring)) {
+		ring_req = RING_GET_REQUEST(&vfb.ring, vfb.ring.req_prod_pvt);
 
-	ring_req = vfb_ring_request(&vfb.ring);
-	if (ring_req) {
 		memcpy(ring_req->buffer, buffer, VFB_PACKET_SIZE);
 
-		vfb_ring_request_ready(&vfb.ring);
+		/* Fill in the ring_req structure */
+
+		/* Make sure the other end "sees" the request when updating the index */
+		dmb();
+
+		vfb.ring.req_prod_pvt++;
+
+		RING_PUSH_REQUESTS(&vfb.ring);
 
 		notify_remote_via_irq(vfb.irq);
 	}
 
-	vfb_end();
+	vdevfront_processing_end();
 }
+#endif
 
-void vfb_probe(void)
-{
-	DBG("vfb init ok\n");
-	write_vbstore();
+void vfb_probe(struct vbus_device *vdev) {
+	int res;
+	unsigned int evtchn;
+	vfb_sring_t *sring;
+	struct vbus_transaction vbt;
+	vfb_t *vfb;
+	grant_ref_t fb_ref;
 
 	DBG0("[" VFB_NAME "] Frontend probe\n");
+
+	if (vdev->state == VbusStateConnected)
+		return ;
+
+
+	vfb = malloc(sizeof(vfb_t));
+	BUG_ON(!vfb);
+	memset(vfb, 0, sizeof(vfb_t));
+
+	dev_set_drvdata(vdev->dev, &vfb->vdevfront);
+
+	DBG("Frontend: Setup ring\n");
+
+	/* Prepare to set up the ring. */
+
+	vfb->ring_ref = GRANT_INVALID_REF;
+
+	/* Allocate an event channel associated to the ring */
+	res = vbus_alloc_evtchn(vdev, &evtchn);
+	BUG_ON(res);
+
+	res = bind_evtchn_to_irq_handler(evtchn, vfb_interrupt, NULL, vdev);
+	if (res <= 0) {
+		lprintk("%s - line %d: Binding event channel failed for device %s\n", __func__, __LINE__, vdev->nodename);
+		BUG();
+	}
+
+	vfb->evtchn = evtchn;
+	vfb->irq = res;
+
+	/* Allocate a shared page for the ring */
+	sring = (vfb_sring_t *) get_free_vpage();
+	if (!sring) {
+		lprintk("%s - line %d: Allocating shared ring failed for device %s\n", __func__, __LINE__, vdev->nodename);
+		BUG();
+	}
+
+	SHARED_RING_INIT(sring);
+	FRONT_RING_INIT(&vfb->ring, sring, PAGE_SIZE);
+
+	/* Prepare the shared to page to be visible on the other end */
+
+	res = vbus_grant_ring(vdev, phys_to_pfn(virt_to_phys_pt((uint32_t) vfb->ring.sring)));
+	if (res < 0)
+		BUG();
+
+	vfb->ring_ref = res;
+
+	vbus_transaction_start(&vbt);
+
+	vbus_printf(vbt, vdev->nodename, "ring-ref", "%u", vfb->ring_ref);
+	vbus_printf(vbt, vdev->nodename, "ring-evtchn", "%u", vfb->evtchn);
+
+	// write something to the vbstore
+	//vbus_write(vbt, "backend/vfb/fb-ph-addr", "value", "value from front-end");
+	fb_ref = vbus_grant_ring(vdev, phys_to_pfn(get_fb_base()));
+	DBG("fb_phys: 0x%08x, fb_ref: 0x%08x\n", get_fb_base(), fb_ref);
+	vbus_printf(vbt, "backend/vfb/fb-ph-addr", "value", "%u", fb_ref);
+
+	vbus_transaction_end(vbt);
 }
 
 /* At this point, the FE is not connected. */
-void vfb_reconfiguring(void)
-{
+void vfb_reconfiguring(struct vbus_device *vdev) {
+	int res;
+	struct vbus_transaction vbt;
+	vfb_t *vfb = to_vfb(vdev);
+
 	DBG0("[" VFB_NAME "] Frontend reconfiguring\n");
+	/* The shared page already exists */
+	/* Re-init */
+
+	gnttab_end_foreign_access_ref(vfb->ring_ref);
+
+	DBG("Frontend: Setup ring\n");
+
+	/* Prepare to set up the ring. */
+
+	vfb->ring_ref = GRANT_INVALID_REF;
+
+	SHARED_RING_INIT(vfb->ring.sring);
+	FRONT_RING_INIT(&vfb->ring, (&vfb->ring)->sring, PAGE_SIZE);
+
+	/* Prepare the shared to page to be visible on the other end */
+
+	res = vbus_grant_ring(vdev, phys_to_pfn(virt_to_phys_pt((uint32_t) vfb->ring.sring)));
+	if (res < 0)
+		BUG();
+
+	vfb->ring_ref = res;
+
+	vbus_transaction_start(&vbt);
+
+	vbus_printf(vbt, vdev->nodename, "ring-ref", "%u", vfb->ring_ref);
+	vbus_printf(vbt, vdev->nodename, "ring-evtchn", "%u", vfb->evtchn);
+
+	vbus_transaction_end(vbt);
 }
 
-void vfb_shutdown(void)
-{
+void vfb_shutdown(struct vbus_device *vdev) {
+
 	DBG0("[" VFB_NAME "] Frontend shutdown\n");
 }
 
-void vfb_closed(void)
-{
+void vfb_closed(struct vbus_device *vdev) {
+	vfb_t *vfb = to_vfb(vdev);
+
 	DBG0("[" VFB_NAME "] Frontend close\n");
+
+	/**
+	 * Free the ring and deallocate the proper data.
+	 */
+
+	/* Free resources associated with old device channel. */
+	if (vfb->ring_ref != GRANT_INVALID_REF) {
+		gnttab_end_foreign_access(vfb->ring_ref);
+		free_vpage((uint32_t) vfb->ring.sring);
+
+		vfb->ring_ref = GRANT_INVALID_REF;
+		vfb->ring.sring = NULL;
+	}
+
+	if (vfb->irq)
+		unbind_from_irqhandler(vfb->irq);
+
+	vfb->irq = 0;
 }
 
-void vfb_suspend(void)
-{
+void vfb_suspend(struct vbus_device *vdev) {
+
 	DBG0("[" VFB_NAME "] Frontend suspend\n");
 }
 
-void vfb_resume(void)
-{
+void vfb_resume(struct vbus_device *vdev) {
+
 	DBG0("[" VFB_NAME "] Frontend resume\n");
 }
 
-int vfb_notify_fn(void *arg)
-{
+#if 0
+int notify_fn(void *arg) {
+
 	while (1) {
 		msleep(50);
 
@@ -126,40 +254,42 @@ int vfb_notify_fn(void *arg)
 		notify_remote_via_irq(vfb.irq);
 
 		vfb_end();
+
 	}
 
 	return 0;
 }
+#endif
 
-void vfb_connected(void)
-{
+void vfb_connected(struct vbus_device *vdev) {
+	vfb_t *vfb = to_vfb(vdev);
+
 	DBG0("[" VFB_NAME "] Frontend connected\n");
 
 	/* Force the processing of pending requests, if any */
-	notify_remote_via_irq(vfb.irq);
+	notify_remote_via_irq(vfb->irq);
 
 	if (!thread_created) {
 		thread_created = true;
-#if 1
-		kernel_thread(vfb_notify_fn, "notify_th", NULL, 0);
+#if 0
+		kernel_thread(notify_fn, "notify_th", NULL, 0);
 #endif
 	}
 }
 
-void write_vbstore(void)
-{
-	struct vbus_transaction vbt;
-	//local_irq_enable();
-	vbus_transaction_start(&vbt);
-	vbus_write(vbt, "backend/vfb/fb-ph-addr", "value", "try2");
-	vbus_transaction_end(vbt);
-	//local_irq_disable();
-}
+vdrvfront_t vfbdrv = {
+	.probe = vfb_probe,
+	.reconfiguring = vfb_reconfiguring,
+	.shutdown = vfb_shutdown,
+	.closed = vfb_closed,
+	.suspend = vfb_suspend,
+	.resume = vfb_resume,
+	.connected = vfb_connected
+};
 
-static int vfb_init(dev_t *dev)
-{
-	vfb_vbus_init();
+static int vfb_init(dev_t *dev) {
 
+	vdevfront_init(VFB_NAME, &vfbdrv);
 
 	return 0;
 }
