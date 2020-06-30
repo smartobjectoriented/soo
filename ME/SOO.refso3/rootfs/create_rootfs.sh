85129a5b0cd08c3d1ce5c8b3118eeab91d5f8646
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-06-30 20:49:58 +0200

    Added missing rootfs in SOO.refso3 ME

---
+++ ME/SOO.refso3/rootfs/create_rootfs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p board/so3virt
+
+#create image first
+dd if=/dev/zero of=board/so3virt/rootfs.fat bs=1024 count=3048
+DEVLOOP=$(sudo losetup --partscan --find --show board/so3virt/rootfs.fat)
+
+#create the partition this way
+(echo o; echo n; echo p; echo; echo; echo; echo; echo; echo t; echo; echo c; echo w) | sudo fdisk $DEVLOOP;
+
+sudo mkfs.vfat ${DEVLOOP}p1
+sudo losetup -d $DEVLOOP
