815bc5bff5737f1e44a0c86fd225a5f2f52cd9e4
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-07-11 10:23:31 +0200

    Supplied two variants of configuration for SO3 ME (with and without ramfs)

--- ME/SOO.refso3/usr/deploy.sh
+++ ME/SOO.refso3/usr/deploy.sh
@@ -3,8 +3,7 @@
 # Deploy usr apps into the first partition
 echo Deploying usr apps into the first partition...
 cd ../rootfs
-./mount_rootfs.sh
+./mount_ramfs.sh so3virt
 sudo rm -rf fs/*
 sudo cp -r ../usr/out/* fs/
-sudo cp -r ../usr/resources/* fs/
-sudo ./umount_rootfs.sh
+sudo ./umount_ramfs.sh so3virt
