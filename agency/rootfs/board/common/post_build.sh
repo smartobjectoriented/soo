df74bfd5b435fd4ce9a52ab333caf85580fe9b01
Daniel Rossier <daniel.rossier@heig-vd.ch>
2020-08-26 21:31:21 +0200

    Fixed issues with buildroot and added syslog-ng for logging

--- agency/rootfs/board/common/post_build.sh
+++ agency/rootfs/board/common/post_build.sh
@@ -4,3 +4,7 @@
 # before the final images (i.e. rootfs.tar) are generated.
 # See post_image.sh script for the latter.
 # Note: you should have access to most BR variables here.
+
+rm -f ${BASE_DIR}/target/etc/init.d/S01logging
+rm -f ${BASE_DIR}/target/etc/init.d/S01syslog
+
