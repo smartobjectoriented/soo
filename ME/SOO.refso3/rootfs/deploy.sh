fb6298c6c55a9a197c9bfceaef8380eb1b674509
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-02-09 18:50:30 +0100

    Removed old ME/

--- ME/SOO.refso3/rootfs/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Deploying the usr apps into the ramfs filesystem..."
-./mount_ramfs.sh $1
-sudo cp -rf ../usr/out/* fs/
-./umount_ramfs.sh $1
--- ME/SOO.refso3/usr/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Deploy usr apps into the first partition
-echo Deploying usr apps into the first partition...
-cd ../rootfs
-./mount_ramfs.sh so3virt
-sudo rm -rf fs/*
-sudo cp -r ../usr/out/* fs/
-sudo ./umount_ramfs.sh so3virt
