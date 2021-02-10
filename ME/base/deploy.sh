b53c426c13cb4c719c682dcd0c48174f5f0ec936
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-02-10 16:59:16 +0100

    Fixed bad description in dts, updated libc in ME/base/usr and updated its

--- ME/base/deploy.sh
+++ ME/base/deploy.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-  echo "Usage: $0 <ME_NAME>"
+usage() {
+  echo "Usage: $0 -m <ME_NAME> <ITB_FILE>"
   echo ""
-  echo "Here, ME name is without the SOO. prefix"
+  echo "ME_NAME can be one of the following:"
+  echo "  - SOO.refso3"
+  echo "  - SOO.blind"
+  echo "  - SOO.outdoor"
+  echo "  - SOO.net"
+  echo "  - SOO.agency"
   echo ""
-  echo "- refso3"
+  echo "The <ITB_FILE> (without extension) depends on the selected .its file available in the target/ directory."
   
   exit 1
+}
+
+while getopts "mc" o; do
+  case "$o" in
+    c)
+      deploy_clean=y
+	  ;;
+    m)
+      deploy_me=y
+	  ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ $OPTIND -eq 1 ]; then usage; fi
+
+if [ "$deploy_me" == "y" ]; then
+        echo Deploying the ME into its itb file...
+        cd target
+        ./mkuboot.sh $3
+
+        echo Copying the ITB image $2.itb in the target $2 directory
+        cp $3.itb ../../$2/target/
 fi
 
-echo Deploying the ME into its itb file...
-cd target
-./mkuboot.sh $1
-echo Copying the ITB image in the target SOO.$1 directory
-cp $1.itb ../../SOO.$1/target/
+if [ "$deploy_clean" == "y" ]; then
 
+        echo "Removing all ITB images in $2"
+        rm ../$2/target/*
+fi
 
 
 
--- ME/base/usr/deploy.sh
+++ ME/base/usr/deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# Deploy usr apps into the first partition
-echo Deploying usr apps into the first partition...
+if [ "$PLATFORM" == "" ]; then
+    if [ "$1" == "" ]; then
+        echo "PLATFORM must be defined (vexpress, rpi4, so3virt)"
+        echo "You can invoke deploy.sh <platform>"
+        exit 0
+    fi
+    
+    PLATFORM=$1
+fi
+echo "Here: board is  ${PLATFORM}"
+echo "------------------- deploy usr apps in so3  ---------------"
+
+
+echo Deploying user apps into the ramfs partition
+
 cd ../rootfs
-./mount_ramfs.sh so3virt
-sudo rm -rf fs/*
-sudo cp -r ../usr/out/* fs/
-sudo ./umount_ramfs.sh so3virt
+./mount_ramfs.sh  ${PLATFORM}
+sudo cp -r ../usr/out/* fs
+./umount_ramfs.sh  ${PLATFORM}
+
