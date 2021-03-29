9757c3e7913ce629bbed82f48eaf8019aaec62db
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-03-29 19:03:29 +0200

    rpi4_64: Fixed bad board in post_image.sh

--- agency/rootfs/board/rpi4_64/post_image.sh
+++ agency/rootfs/board/rpi4_64/post_image.sh
@@ -5,5 +5,5 @@
 # See post_build.sh script if you want to do something just before
 # the images are generated.
 # Note: you should have access to most BR variables here.
-cp ${BASE_DIR}/images/rootfs.cpio ${BASE_DIR}/board/rpi4
+cp ${BASE_DIR}/images/rootfs.cpio ${BASE_DIR}/board/rpi4_64
 
