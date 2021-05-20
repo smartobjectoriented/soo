775bcbf7aed84134951bda6fb143423832190a5f
David Truan <david.truan@heig-vd.ch>
2021-05-20 13:13:22 +0200

    [RPi4]: Fix btmanager.sh.

--- agency/rootfs/board/rpi4/rootfs_overlay/root/btmanager.sh
+++ agency/rootfs/board/rpi4/rootfs_overlay/root/btmanager.sh
@@ -4,9 +4,9 @@ BT_TTY="/dev/ttyAMA1"
 
 # The MAC address is generated using some agency UID bytes
 AGENCYUID_FILE="/sys/devices/system/soo/soo0/agencyUID"
-BDADDR=`cat ${AGENCYUID_FILE} | cut -c0-17`
+BDADDR=`cat ${AGENCYUID_FILE} | cut -c1-17`
 
-hciattach ${BT_TTY} bcm2035 3000000 flow - ${BDADDR}
+hciattach ${BT_TTY} bcm2035 115200 flow - ${BDADDR}
 
 DEFAULT_HCI_NAME="soo-bt"
 
@@ -17,17 +17,31 @@ else
     HCI_NAME=${DEFAULT_HCI_NAME}
 fi
 
-/usr/libexec/bluetooth/bluetoothd &
+/usr/libexec/bluetooth/bluetoothd --compat &
 hciconfig hci0 up
-hciconfig hci0 name ${HCI_NAME}
+# Secure Simple Pairing Mode
+hciconfig hci0 sspmode 1
 # Discoverable
 hciconfig hci0 piscan
 # Class Networking
-# Secure Simple Pairing Mode
-hciconfig hci0 sspmode 1
-# Class Networking
-hciconfig hci0 class 020300
+hciconfig hci0 class 001101
+hciconfig hci0 name ${HCI_NAME}
+# Serial Port
+sdptool add --channel=1 SP
 
+# Launch the agent with NoInputNoOutput so we don't have to enter a PIN 
+bt-agent -c NoInputNoOutput -d
+
+
+# This opens a rfcomm socket which will be hooked by vuihandler
+while true
+do
+    # Kill RFCOMM instance if any
+    killall -9 rfcomm > /dev/null 2>&1
+    rfcomm release all
+    # Watch for RFCOMM connections
+    rfcomm -r watch 1
+done
 
 
 
