26f0f7f5716ffb7251cef4d4109cdb24614a9e6c
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-09-06 21:21:38 +0200

    [rpi4] Updated wifi manager handling via udev

--- agency/rootfs/board/rpi4/rootfs_overlay/root/wifimanager.sh
+++ agency/rootfs/board/rpi4/rootfs_overlay/root/wifimanager.sh
@@ -1,27 +1,6 @@
-#!/bin/bash
-
-DEFAULT_CHANNEL=36
-DEFAULT_SSID="soo-wifi"
-
-if [[ -n ${WIFI_SSID} ]]
-then
-    SSID=${WIFI_SSID}
-else
-    SSID=${DEFAULT_SSID}
-fi
-
-if [[ -n ${WIFI_CHANNEL} ]]
-then
-    CHANNEL=${WIFI_CHANNEL}
-else
-    CHANNEL={DEFAULT_CHANNEL}
-fi
-
+#!/bin/sh
 iw reg set CH
-iwconfig wlan0 mode ad-hoc
-iwconfig wlan0 essid ${SSID}
-iwconfig wlan0 channel ${DEFAULT_CHANNEL}
-sleep 2
+iw wlan0 set type ibss
 ifconfig wlan0 up
-sleep 1
-
+iw wlan0 ibss join soo-wifi 5180
+iwconfig wlan0 essid "soo-wifi"
