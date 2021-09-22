6cf42984285fa68827fa428af271f5c6b4fc6b4a
Daniel Rossier <daniel.rossier@heig-vd.ch>
2021-09-22 12:12:45 +0200

    Removed old MERIDA sunxi related code and various small improvements

--- agency/linux/mm/init-mm.c
+++ agency/linux/mm/init-mm.c
@@ -28,7 +28,7 @@
  */
 struct mm_struct init_mm = {
 	.mm_rb		= RB_ROOT,
- 	.pgd		= swapper_pg_dir,
+	.pgd		= swapper_pg_dir,
 	.mm_users	= ATOMIC_INIT(2),
 	.mm_count	= ATOMIC_INIT(1),
 	.mmap_sem	= __RWSEM_INITIALIZER(init_mm.mmap_sem),
