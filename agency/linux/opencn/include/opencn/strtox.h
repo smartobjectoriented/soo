2397a5b2d65cbdcb79c300eadc8710ce8668beb7
Daniel Rossier <daniel.rossier@heig-vd.ch>
2022-02-09 17:35:27 +0100

    Removed opencn stuff

--- agency/linux/opencn/include/opencn/strtox.h
+++
@@ -1,8 +0,0 @@
-
-
-double strtod(char *s, char **endptr);
-unsigned long long strtoull(char *s, char **endptr, int base);
-long long strtoll(char *s, char **endptr, unsigned int base);
-unsigned long strtoul(char *s, char **endptr, unsigned int base);
-unsigned int strtouint(char *s, char **endptr, unsigned int base);
-int strtol(const char *s, char **endptr, unsigned int base);
