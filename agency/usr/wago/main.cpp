4c605d61c9a0e24376682952c4fbb246be56af3a
mgi-reds <mattia.gallacchi@heig-vd.ch>
2022-03-04 10:46:47 +0100

    [WAGO] Add user application to control wago leds
    Signed-off-by: mgi-reds <mattia.gallacchi@heig-vd.ch>

---
+++ agency/usr/wago/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <unistd.h>
+#include "http.h"
+
+#define SLEEP               1000000
+#define BASE_ADDR           "192.168.1.10"
+#define PORT                "8080"
+#define GET_TOPOLOGY        "dali/topology"
+#define GET_STATUS          "dali/led"
+#define POST_LED_ON         "dali/led/on"
+#define POST_LED_OFF        "dali/led/off"
+
+
+int main()
+{
+    HTTP::Request client(BASE_ADDR, PORT);
+
+    std::string resp;
+
+    // if (client.GET(GET_TOPOLOGY, resp) < 0) {
+    //     std::cout << "Failed to read topology" << std::endl;
+    // } else {
+    //     std::cout << "get topology :\n" << resp << std::endl;
+    // }
+
+    // if (client.GET(GET_STATUS, resp, "?id=all") < 0) {
+    //     std::cout << "Failed to read status" << std::endl;
+    // } else {
+    //     std::cout << "get status :\n" << resp << std::endl;
+    // }
+
+    client.POST(POST_LED_OFF, "?id=all");
+
+    int id = 1;
+    std::string args;
+
+    while (1)
+    {
+        args = "?id=";
+        for (int i = 0; i < 6; i++)
+        {
+            args += std::to_string(id + i) + ",";
+        }
+        // args = "?id=" + std::to_string(id);
+        
+        if (client.POST(POST_LED_ON, args) < 0) 
+        {
+            std::cout << "Failed to POST led on" << std::endl;
+            break;
+        }
+
+        usleep(SLEEP);
+
+        if (client.POST(POST_LED_OFF, args) < 0)
+        {
+            std::cout << "Failed to POST led on" << std::endl;
+            break;
+        }
+
+        id += 6;
+        if (id > 12) {
+            id = 1;
+        } 
+    }
+    
+
+    return 0;
+}
