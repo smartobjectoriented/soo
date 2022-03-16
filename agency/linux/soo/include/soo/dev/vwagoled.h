4331571f499fa28489ea8ca1830005a6ef6a4dea
mgi-reds <mattia.gallacchi@heig-vd.ch>
2022-03-16 16:23:11 +0100

    [WAGO] Add wagoled driver
    Signed-off-by: mgi-reds <mattia.gallacchi@heig-vd.ch>

---
+++ agency/linux/soo/include/soo/dev/vwagoled.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2014-2019 Daniel Rossier <daniel.rossier@heig-vd.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef VWAGOLED_H
+#define VWAGOLED_H
+
+#include <soo/ring.h>
+#include <soo/grant_table.h>
+#include <soo/vdevback.h>
+
+#define VWAGOLED_PACKET_SIZE	32
+
+#define VWAGOLED_NAME		"vwagoled"
+#define VWAGOLED_PREFIX		"[" VWAGOLED_NAME "] "
+
+typedef struct {
+	/* nothing */
+} vwagoled_request_t;
+
+typedef struct  {
+
+    // Max dali devices 64
+	uint8_t id;
+
+    // ON/OFF
+    uint8_t status;
+
+    // Dim value 0-255
+	uint8_t value;
+
+} vwagoled_response_t;
+
+/*
+ * Generate ring structures and types.
+ */
+DEFINE_RING_TYPES(vwagoled, vwagoled_request_t, vwagoled_response_t);
+
+/*
+ * General structure for this virtual device (backend side)
+ */
+
+typedef struct {
+
+	/* Must be the first field */
+	vdevback_t vdevback;
+
+	vwagoled_back_ring_t ring;
+	unsigned int irq;
+
+} vwagoled_t;
+
+#endif /* VWAGOLED_H */
