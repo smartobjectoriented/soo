6754a5c77b2df9a33fec286c596f2a17c1e611c7
mgi-reds <mattia.gallacchi@heig-vd.ch>
2022-03-17 18:02:09 +0100

    [WAGO USR APP] Add methods to access sysfs attributes to interact with kernel driver
    Signed-off-by: mgi-reds <mattia.gallacchi@heig-vd.ch>

--- agency/usr/wago/ledcontrol.h
+++ agency/usr/wago/ledcontrol.h
@@ -3,13 +3,18 @@
 #include <vector>
 #include "http.h"
 
+
+/**** REST SERVER DATA ****/
 #define BASE_ADDR           "192.168.1.10"
 #define PORT                "8080"
 #define GET_TOPOLOGY        "dali/topology"
 #define GET_STATUS          "dali/led"
 #define POST_LED_ON         "dali/led/on"
 #define POST_LED_OFF        "dali/led/off"
+#define ARGS                "?id="
+#define LED_ALL             "all"
 
+/**** JSON DATA ****/
 #define DATA_OBJ            "Data"
 #define DEVICE_ARR          "devices"
 #define DEVICE_TYPE         "type"
@@ -18,11 +23,36 @@
 #define LED_STATUS          "status"
 #define LED_ON              "on"
 
-#define ARGS                "?id="
-#define LED_ALL             "all"
+/**** DRIVER SYSFS ACCESS ****/
+#define SYSFS_BASE          "/sys/soo/backend/vwagoled/vwagoled_"
+#define SYSFS_NOTIFY        SYSFS_BASE "notify"
+#define SYSFS_DEBUG         SYSFS_BASE "debug"
+
+/**** BUFFERS ****/
+#define BUFFER_SIZE         1024
+#define CMD_SIZE            32
+#define CMD_NUM             5
+
+#define IDS_DELIM           ','
 
 namespace LED
 {
+    typedef enum 
+    {
+        CMD_LED_ON = 0,
+        CMD_LED_OFF,
+        CMD_GET_STATUS,
+        CMD_GET_TOPOLOGY,
+        NONE
+    } wagoled_cmd_t;
+
+    static const char notify_str [][CMD_SIZE] = {
+        "led_on",
+        "led_off",
+        "get_status",
+        "get_topology",
+        "none"
+    };
 
     struct led_t
     {
@@ -37,6 +67,10 @@ namespace LED
         std::vector<led_t*> leds;
         HTTP::Request client;
         void build_args_str(std::string& args, std::vector<int> ids);
+        void start_debug();
+        void stop_debug();
+        void extract_ids(char *ids, std::vector<int>& vect_ids);
+        int process_notify(char *cmd, char* ids);
 
     public:
         Ledctrl();
@@ -47,6 +81,8 @@ namespace LED
         int turn_off(std::vector<int> ids);
         int turn_all_on();
         int turn_all_off();
+
+        int main_loop();
     };
 
 } // end LED
--- agency/usr/wago/main.cpp
+++ agency/usr/wago/main.cpp
@@ -6,7 +6,7 @@
 #define MSEC                1000
 #define SLEEP               1 * SEC
 
-int main(int argc, char *argv[])
+void test_func()
 {
     int count = 0;
     std::vector<int> ids{1, 2, 3, 4, 5, 6};
@@ -43,57 +43,13 @@ int main(int argc, char *argv[])
         
         count++;
     }
-    
-    /* HTTP::Request client(BASE_ADDR, PORT);
+}
 
-    std::string resp;
+int main(int argc, char *argv[])
+{
+    LED::Ledctrl ctl;
 
-    // if (client.GET(GET_TOPOLOGY, resp) < 0) {
-    //     std::cout << "Failed to read topology" << std::endl;
-    // } else {
-    //     std::cout << "get topology :\n" << resp << std::endl;
-    // }
+    ctl.init();
 
-    // if (client.GET(GET_STATUS, resp, "?id=all") < 0) {
-    //     std::cout << "Failed to read status" << std::endl;
-    // } else {
-    //     std::cout << "get status :\n" << resp << std::endl;
-    // }
-
-    client.POST(POST_LED_OFF, "?id=all");
-
-    int id = 1;
-    std::string args;
-
-    while (1)
-    {
-        args = "?id=";
-        for (int i = 0; i < 6; i++)
-        {
-            args += std::to_string(id + i) + ",";
-        }
-        // args = "?id=" + std::to_string(id);
-        
-        if (client.POST(POST_LED_ON, args) < 0) 
-        {
-            std::cout << "Failed to POST led on" << std::endl;
-            break;
-        }
-
-        usleep(SLEEP);
-
-        if (client.POST(POST_LED_OFF, args) < 0)
-        {
-            std::cout << "Failed to POST led on" << std::endl;
-            break;
-        }
-
-        id += 6;
-        if (id > 12) {
-            id = 1;
-        } 
-    } */
-    
-
-    return 0;
+    return ctl.main_loop();
 }
