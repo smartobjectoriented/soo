6116ac79429ac6e25717ab62f3be8af05b957cfc
mgi-reds <mattia.gallacchi@heig-vd.ch>
2022-03-18 14:10:29 +0100

    [ME WAGO] Add SOO.wagoled Mobile Entity
    Signed-off-by: mgi-reds <mattia.gallacchi@heig-vd.ch>

---
+++ ME/base/so3/soo/include/me/wagoled.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2022 Mattia Gallacchi <mattia.gallaccchi@heig-vd.ch>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef WAGOLED_H
+#define WAGOLED_H
+
+#include <completion.h>
+#include <spinlock.h>
+
+#include <me/common.h>
+
+/*
+ * Never use lock (completion, spinlock, etc.) in the shared page since
+ * the use of ldrex/strex instructions will fail with cache disabled.
+ */
+typedef struct {
+
+	/* Current letter */
+	char cur_letter;
+
+	/*
+	 * MUST BE the last field, since it contains a field at the end which is used
+	 * as "payload" for a concatened list of hosts.
+	 */
+	me_common_t me_common;
+
+} sh_wagoled_t;
+
+/* Export the reference to the shared content structure */
+extern sh_wagoled_t *sh_wagoled;
+
+#endif /* WAGOLED_H */
+
+
