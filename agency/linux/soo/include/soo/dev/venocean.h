2f5edf6aa84a761ba51eb0878e0925a6c6fe151e
mgi-reds <mattia.gallacchi@heig-vd.ch>
2022-04-04 11:20:14 +0200

    [WAGOLED] Add TCM515 serdev driver, esp3 protocol, venocean and vwagoled backends
    Signed-off-by: mgi-reds <mattia.gallacchi@heig-vd.ch>

--- agency/linux/soo/include/soo/dev/venocean.h
+++ agency/linux/soo/include/soo/dev/venocean.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2016-2018 Baptiste Delporte <bonel@bonel.net>
  * Copyright (C) 2018-2019 Daniel Rossier <daniel.rossier@heig-vd.ch>
+ * Copyright (C) 2022 Mattia Gallacchi <mattia.gallacchi@heig-vd.ch>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -17,15 +18,29 @@
  *
  */
 
-/**** DTS ****
- * 	
- * 	venocean {
- *		compatible = "venocean,backend";
- *		status = "ok";
- *
- *		SW2-gpios = <&gpio 0 GPIO_ACTIVE_HIGH>;
- *		SW1-gpios = <&gpio 1 GPIO_ACTIVE_HIGH>;
- *	};
+/* ============================== DTS =================================== */
+/*
+soo.dtsi:
+
+ 	venocean {
+		compatible = "venocean,backend";
+		status = "disabled";
+	};
+ 
+ To activate add to bcm2711-<platform>.dts:
+	
+	&agency {
+		backends {
+			...
+
+			venocean {
+				status = "ok";
+			};
+
+			...
+		};
+	};
+ 
  */
 
 
@@ -38,59 +53,21 @@
 #include <linux/vt_kern.h>
 
 #define VENOCEAN_NAME	"venocean"
-#define VENOCEAN_PREFIX	" [" VENOCEAN_NAME "] "
+#define VENOCEAN_PREFIX	" [" VENOCEAN_NAME " backend ]"
 
-/*** UART ***/
-#define ENOCEAN_UART_DEV 		"ttyAMA1"
-#define ENOCEAN_UART_BAUD		57600
-#define ENOCEAN_UART_BITS		8
-#define ENOCEAN_UART_PARITY		'n'
-#define ENOCEAN_UART_FLOW		'n'
-
-#define VENOCEAN_FRAME_SIZE 20
-
-#define VENOCEAN_INIT_SIZE 5
-
-//different states for the switchs (single channel)
-//Interpreted from the frame during successive tests (could not find documentation about the data encription)
-#define SWITCH_IS_RELEASED 0x00 
-#define SWITCH_IS_UP 0x70
-#define SWITCH_IS_DOWN 0x50
-
-#define ENOCEAN_MODE_BLIND 0
-#define ENOCEAN_MODE_VALVE 1
-
-#define UP_GPIO		26
-
-#define DOWN_GPIO	16
-
-/* GPIO polling period expressed in ms */
-#define GPIO_POLL_PERIOD	10
-
-/*  This is a reserved char code we use to query (patched) Qemu to retrieve the window size. */
-#define SERIAL_GWINSZ   '\254'
+/*** For now it should be enough ***/
+#define BUFFER_SIZE		256
 
 typedef struct {
-	char c;
+	unsigned char buffer[BUFFER_SIZE];
+	/* Buffer len */
+	int len;
 } venocean_request_t;
 
 typedef struct {
-	char c;
+	unsigned char buffer[BUFFER_SIZE];
+	int len;
 } venocean_response_t;
-
-/* ASCII data coming from the enocean module*/
-typedef struct {
-	char	frame_begin;	/* 0x55 */
-	char	data_length[2];	/* lenght for the main datas*/
-	char	optionnal_length;	/* length for the optionnal datas */
-	char	packet_type;	/* 0x01 = ERP Radio Telegramm */
-	char	crc_h;	/* CRC 8 */
-	char	command_code;	/* command code */
-	char	switch_data;	/* switch state at telegramm reception*/
-	char	switch_ID[4];	/* enocean device ID */
-	char	secondary_datas[7];	/* secondary datas from the switch */
-	char	crc_l;	/* CRC 8*/
-} venocean_ascii_data_t;
 
 DEFINE_RING_TYPES(venocean, venocean_request_t, venocean_response_t);
 
@@ -106,13 +83,5 @@ typedef struct {
 	unsigned int irq;
 
 } venocean_t;
-
-bool venocean_ready(void);
-
-static inline venocean_t *to_venocean(struct vbus_device *vdev) {
-	vdevback_t *vdevback = dev_get_drvdata(&vdev->dev);
-	return container_of(vdevback, venocean_t, vdevback);
-}
-
 
 #endif /* VENOCEAN_H */
