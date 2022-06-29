7fc63e5854afcec795f4b7e8d6651027502c9a71
David Truan <david.truan@heig-vd.ch>
2022-06-29 15:31:27 +0200

    [agency]: Remove hciattach from btmanager.sh

--- agency/rootfs/board/rpi4_64/rootfs_overlay/root/btmanager.sh
+++ agency/rootfs/board/rpi4_64/rootfs_overlay/root/btmanager.sh
@@ -11,7 +11,7 @@ echo "Bluetooth configuration..."
 echo "- with MAC:" ${AGENCYUID}
 
 # Configure the controller with an address and load its firmware
-hciattach ${BT_TTY} bcm43xx 460800 flow - ${BDADDR}
+# hciattach ${BT_TTY} bcm43xx 460800 flow - ${BDADDR}
 
 DEFAULT_HCI_NAME="soo-bt"
 
