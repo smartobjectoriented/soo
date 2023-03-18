af574d54fb7b0f02344713dca9ac64b7b307a386
Daniel Rossier <daniel.rossier@heig-vd.ch>
2023-03-18 17:28:10 +0100

    Fixed regression in AVZ

--- ME/base/so3/arch/arm64/virt64/platsmp.c
+++ ME/base/so3/arch/arm64/virt64/platsmp.c
@@ -35,7 +35,7 @@ static DEFINE_SPINLOCK(cpu_lock);
 void smp_boot_secondary(unsigned int cpu)
 {
 	spin_lock(&cpu_lock);
-	cpu_on(cpu, (u32) __pa(secondary_startup));
+	cpu_on(cpu, (addr_t) __pa(secondary_startup));
 	spin_unlock(&cpu_lock);
 }
 
