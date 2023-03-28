5845fb8d48637abe0f56b89f159143502d168769
act-reds <arzur.cateltorres@heig-vd.ch>
2023-03-28 10:58:37 +0200

    fix ME compilation

--- ME/base/so3/soo/kernel/soo_guest_activity.c
+++ ME/base/so3/soo/kernel/soo_guest_activity.c
@@ -291,12 +291,7 @@ void perform_task(dc_event_t dc_event)
 void do_soo_activity(void *arg)
 {
 	soo_domcall_arg_t *args = (soo_domcall_arg_t *) arg;
-
-<<<<<<< HEAD
-=======
-	//lprintk("[%s] here we are\n", __func__);
-
->>>>>>> 43d67c4bb1 (rebase)
+	
 	switch (args->cmd) {
 
 	case CB_PRE_SUSPEND: /* Called by perform_pre_suspend */
