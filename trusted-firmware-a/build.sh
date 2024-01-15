00daa6110c1079670363c4406dcb275c830f1d4d
Daniel Rossier <daniel.rossier@heig-vd.ch>
2024-01-15 12:04:49 +0100

    Added bbb_defconfig

--- trusted-firmware-a/build.sh
+++ trusted-firmware-a/build.sh
@@ -7,11 +7,11 @@ while read var; do
 if [ "$var" != "" ]; then
   export $(echo $var | sed -e 's/ //g' -e /^$/d -e 's/://g' -e /^#/d)
 fi
-done < ../agency/build.conf
+done < ../build.conf
 
-if [ "$PLATFORM" == "merida" ]; then
-    echo "Building ATF for running on MERIDA"
-    make  CROSS_COMPILE=aarch64-linux-gnu- PLAT=sun50i_a64 DEBUG=1 SPD=opteed bl31 -j8
+if [ "$PLATFORM" == "virt64" ]; then
+    echo "Building ATF for running on QEMU/virt64"
+    make  CROSS_COMPILE=aarch64-none-linux-gnu- PLAT=qemu DEBUG=1 SPD=opteed bl31 -j8
 fi
 
 if [ "$PLATFORM" == "vexpress" ]; then
