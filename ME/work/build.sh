875892a19526bd4489099f20103e28ded1002205
Daniel Rossier <daniel.rossier@heig-vd.ch>
2024-03-23 09:40:05 +0100

    WiP: Fixing soo/so3 files

---
+++ ME/work/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo Building symlinks with mainline SO3 (it takes a while!)...
+
+# Source directory containing files to link from
+source_dir="${PWD}/../so3"
+
+# Destination directory where symlinks will be created
+destination_dir="${PWD}/so3"
+
+# Change to the script's directory to resolve relative paths
+cd "$(dirname "$0")" || exit
+
+# Create destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Loop through each file in the source directory
+find "$source_dir" -type f -exec bash -c '
+for file do
+    # Get relative path of the file
+    relative_path="${file#$1}"
+
+    # Ensure destination directory structure exists
+    mkdir -p "$2/${relative_path%/*}"
+
+    # Check if the file already exists in the destination directory
+    if [ ! -e "$2/$relative_path" ]; then
+        # Create symlink
+        ln -s "$file" "$2/$relative_path"
+    fi
+done
+' bash "$source_dir" "$destination_dir" {} +
+
+echo Remove .git* files in the root...
+rm -rf so3/.git*
+
+echo Now, building symlinks for overlays...
+
+# Source directory containing files to link from
+source_dir="${PWD}/../soo/so3"
+
+# Destination directory where symlinks will be created
+destination_dir="${PWD}/so3"
+
+# Change to the script's directory to resolve relative paths
+cd "$(dirname "$0")" || exit
+
+# Create destination directory if it doesn't exist
+mkdir -p "$destination_dir"
+
+# Loop through each file in the source directory
+find "$source_dir" -type f -exec bash -c '
+for file do
+    # Get relative path of the file
+    relative_path="${file#$1}"
+
+    # Ensure destination directory structure exists
+    mkdir -p "$2/${relative_path%/*}"
+
+    # Check if the file already exists in the destination directory
+    if [ ! -e "$2/$relative_path" ]; then
+        # Create symlink
+        ln -fs "$file" "$2/$relative_path"
+    fi
+done
+' bash "$source_dir" "$destination_dir" {} +
+
